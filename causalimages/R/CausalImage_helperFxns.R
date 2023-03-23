95e4d57205ab60a7ce0a26d39d36e2af1f7de09f
cjerzak <connor.jerzak@gmail.com>
2023-03-23 10:58:10 -0500

    added image2

--- causalimages/R/CausalImage_helperFxns.R
+++ causalimages/R/CausalImage_helperFxns.R
@@ -1,23 +1,40 @@
-
 #!/usr/bin/env Rscript
-#' Matrix visualization as heatmap with correct north-south-east-east orientation
+#' Visualizing matrices as heatmaps with correct north-south-east-west orientation
 #'
 #' A function for generating a heatmap representation of a matrix with correct spatial orientation.
 #'
 #' @usage
 #'
-#' image2( x, ... )
+#' image2( x )
 #'
-#' @param x A numeric matrix.
-#' @param obsY A numeric
+#' @param x (required) The numeric matrix to be visualized.
+#' @param xaxt (default = `""`) The x-axis label.
+#' @param yaxt (default = `""`) The y-axis label.
+#' @param main (default = `""`) The main figure label.
+#' @param main.cex (default = `1.`) The main figure label sizing factor.
+#' @param box (default = `F`) Should a box be plotted around the image?
 #'
-#' @return A heatmap representation of the matrix, `x`, with correct north/south/east/west orientation.
+#' @return Returns a heatmap representation of the matrix, `x`, with correct north/south/east/west orientation.
 #'
-image2 = function(mat,xaxt=NULL,yaxt = NULL,main=NULL,cex.main = NULL,box=F){
-  image((t(mat)[,nrow(mat):1]), axes = F, main = main,xaxs = "i",cex.main = cex.main)
+#' @examples
+#' #set seed
+#' set.seed(1)
+#'
+#' #Geneate data
+#' x <- matrix(rnorm(50*50), ncol = 50)
+#' diag(x) <- 3
+#'
+#' # create plot
+#' image2(x, main = "Example Text", cex.main = 2)
+#'
+#' @export
+#' @md
+#'
+image2 = function(x,xaxt=NULL,yaxt = NULL,main=NULL,cex.main = NULL,box=F){
+  image((t(x)[,nrow(x):1]), axes = F, main = main,xaxs = "i",cex.main = cex.main)
   if(box == T){box()}
-  if(!is.null(xaxt)){ axis(1, at = 0:(nrow(mat)-1)/nrow(mat)*1.04, tick=F,labels = (xaxt),cex.axis = 1,las = 1)  }
-  if(!is.null(yaxt)){ axis(2, at = 0:(nrow(mat)-1)/nrow(mat)*1.04, tick=F,labels = rev(yaxt),cex.axis = 1,las = 2)  }
+  if(!is.null(xaxt)){ axis(1, at = 0:(nrow(x)-1)/nrow(x)*1.04, tick=F,labels = (xaxt),cex.axis = 1,las = 1)  }
+  if(!is.null(yaxt)){ axis(2, at = 0:(nrow(x)-1)/nrow(x)*1.04, tick=F,labels = rev(yaxt),cex.axis = 1,las = 2)  }
 }
 
 # zips two lists
