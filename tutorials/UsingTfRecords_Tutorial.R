c36c036b32b2250c195e25c54087076a45355bad
cjerzak <connor.jerzak@gmail.com>
2023-09-04 08:22:07 -0700

    docs updated

--- tutorials/UsingTfRecords_Tutorial.R
+++ tutorials/UsingTfRecords_Tutorial.R
@@ -52,15 +52,15 @@ take_indices <- unlist( tapply(1:length(obsW),obsW,function(zer){sample(zer, 50)
 # uncomment for a larger n analysis
 #take_indices <- 1:length( obsY )
 
-# set tfrecord save location (use absolute paths)
-#tfrecord_loc <- "~/Downloads/ExampleRecord.tfrecord"
+# set tfrecord save location
+tfrecord_loc <- "~/Downloads/ExampleRecord.tfrecord"
 
-tfrecord_loc <- "ExampleRecord.tfrecord"
+#tfrecord_loc <- "ExampleRecord.tfrecord"
 
-# don't use relative paths like below:
+# you may use relative paths like this:
 #tfrecord_loc <- "./Downloads/test1/test2/test3/ExampleRecord.tfrecord"
 
-# instead use absolute paths
+# or absolute paths like this:
 #tfrecord_loc <- "~/Downloads/test1/test2/test3/ExampleRecord.tfrecord"
 
 # write a tf records repository
@@ -103,8 +103,8 @@ ImageConfoundingAnalysis <- AnalyzeImageConfounding(
   imageKeysOfUnits = KeysOfObservations[ take_indices ],
   file = tfrecord_loc, # point to tfrecords file
   acquireImageFxn = NULL,
-  modelClass = "cnn", # uses convolutional network (richer model class)
-  #modelClass = "embeddings", # uses image embeddings (faster)
+  #modelClass = "cnn", # uses convolutional network (richer model class)
+  modelClass = "embeddings", # uses image embeddings (faster)
   plotBands = c(1,2,3),
   dropoutRate = 0.1,
   tagInFigures = T, figuresTag = "TutorialExample",
