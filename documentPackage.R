2fabfdefed5eabeb90824df985f7d878aa439287
Connor Jerzak <cjerzak@Connors-MacBook-Pro.local>
2024-07-04 09:36:38 -0400

    add glmnet to required packages

--- documentPackage.R
+++ documentPackage.R
@@ -1,20 +1,20 @@
 {
-rm(list=ls())
-package_name <- "causalimages"
-setwd(sprintf("~/Documents/%s-software", package_name))
-
-package_path <- sprintf("~/Documents/%s-software/%s",package_name,package_name)
-tools::add_datalist(package_path, force = TRUE)
-devtools::document(package_path)
-try(file.remove(sprintf("./%s.pdf",package_name)),T)
-system(sprintf("R CMD Rd2pdf %s",package_path))
-
-# install.packages( sprintf("~/Documents/%s-software/%s",package_name,package_name),repos = NULL, type = "source")
-# library( causalimages ); data(  CausalImagesTutorialData )
-log(sort( sapply(ls(),function(l_){object.size(eval(parse(text=l_)))})))
-
-# Check package to ensure it meets CRAN standards.
-# devtools::check( package_path )
-
-# see https://github.com/RConsortium/S7
+  rm(list=ls())
+  package_name <- "causalimages"
+  setwd(sprintf("~/Documents/%s-software", package_name))
+  
+  package_path <- sprintf("~/Documents/%s-software/%s",package_name,package_name)
+  tools::add_datalist(package_path, force = TRUE)
+  devtools::document(package_path)
+  try(file.remove(sprintf("./%s.pdf",package_name)),T)
+  system(sprintf("R CMD Rd2pdf %s",package_path))
+  
+  # install.packages( sprintf("~/Documents/%s-software/%s",package_name,package_name),repos = NULL, type = "source")
+  # library( causalimages ); data(  CausalImagesTutorialData )
+  log(sort( sapply(ls(),function(l_){object.size(eval(parse(text=l_)))})))
+  
+  # Check package to ensure it meets CRAN standards.
+  # devtools::check( package_path )
+  
+  # see https://github.com/RConsortium/S7
 }
