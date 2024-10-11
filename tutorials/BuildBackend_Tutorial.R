aa02389a185fcd0c0d82d15fc6de09c314adce5a
Connor Jerzak <cjerzak@Connors-MBP-2.cable.rcn.com>
2024-10-10 20:10:57 -0400

    buildbackend edits'

--- tutorials/BuildBackend_Tutorial.R
+++ tutorials/BuildBackend_Tutorial.R
@@ -6,10 +6,17 @@
   # local install for development team
   # install.packages("~/Documents/causalimages-software/causalimages",repos = NULL, type = "source",force = F)
 
-  # setup backend, conda points to location of python in conda where packages will be downloaded
-  # note: This function requires an Internet connection
-  # you can find out a list of conda Python paths via:
-  # system("which python")
-  causalimages::BuildBackend(conda = "/Users/cjerzak/miniforge3/bin/python")
+  # in general, you will simply use: 
+  causalimages::BuildBackend()
+  
+  # Note: This function requires an Internet connection
+  # Note: With default arguments, a conda environment called 
+  # "CausalImagesEnv" will be created with required packages saved within. 
+  
+  # Advanced tip: 
+  # if you need to points to the location of a specific version of python
+  # in conda where packages will be downloaded, 
+  # you can use: 
+  #causalimages::BuildBackend(conda = "/Users/cjerzak/miniforge3/bin/python")
 }
 
